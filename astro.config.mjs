f71b92b7238381ff7d18051d24c8556b2e446432
VarelaG <varelag1999@gmail.com>
2025-03-28 20:59:02 -0300

    Despliegue de pagina

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://VarelaG.github.io',
+  base: 'Porsche',
   vite: {
     plugins: [tailwindcss()]
   }
